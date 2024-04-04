c090defe85f4694703c3c7da06bc0f94d82adca3
Alex <alexmichel249@gmail.com>
2024-04-04 03:53:56 -0700

    upd spent usd

--- main.js
+++ main.js
@@ -120,7 +120,7 @@ function updateHISTORYtab(tab) {
       "=X3+FLOW!X2", //FISH
       "=Y3+FLOW!Y2", 
       "=Z3+FLOW!Z2", 
-      "=AA3+FLOW!AA2", //[SPENT USD]
+      "=0", //[SPENT USD]
       "=AB3+FLOW!AB2", 
       "=AC3+FLOW!AC2", 
       "=AD3+FLOW!AD2", 
@@ -133,45 +133,8 @@ function updateHISTORYtab(tab) {
       "=AK3+FLOW!AK2", 
       "=AL3+FLOW!AL2", 
       "=AM3+FLOW!AM2", 
-      // //TOTALS
-      // "=Y3+FLOW!AA2", //Y SPENT USD
-      // "=ROUND(SUM(AB2:AX2),0)", //Z BALANCE USD
-      // //USD VALUES
-      // "", //AA DATE
-      // "=B2*CURRENCY!B2", //AB USDT value
-      // "=C2*CURRENCY!C2", //AC TON $ value
-      // "=D2*CURRENCY!D2", //AD BTC $ value
-      // "=E2*CURRENCY!E2", //AE ETH $ value
-      // "=F2*CURRENCY!F2", //AF DOGE $ value
-      // "=G2*CURRENCY!G2*CURRENCY!C2", //AG TAN $ value
-      // "=H2*CURRENCY!H2", //AMBR $ value
-      // "=I2*CURRENCY!I2", //GLINT $ value
-      // "=J2*CURRENCY!J2", //JETTON $ value
-      // "=K2*CURRENCY!K2", //LAVE $ value
-      // "=L2*CURRENCY!L2", //PUNK $ value
-      // "=M2*CURRENCY!M2", //RAFF $ value
-      // "=N2*CURRENCY!N2", //JWBTC $ value
-      // "=O2*CURRENCY!O2", //TSTON $ value
-      // "=P2*CURRENCY!P2", //jUSDT $ value
-      // "=Q2*CURRENCY!Q2", //KINGY $ value
-      // "=R2*CURRENCY!R2", //SCALE $ value
-      // "=S2*CURRENCY!S2", //ARBUZ $ value
-      // "=T2*CURRENCY!T2", //TONNEL $ value
-      // "=U2*CURRENCY!U2", //DFC $ value
-      // "=V2*CURRENCY!V2", //LP $ value
-      // "=W2*CURRENCY!W2", //Dogwifhoodie $ value
-      // "=X2*CURRENCY!X2", //FISH $ value
     ],
   ]);
-
-  //direct shitcoins values
-  //const range = tab.getRange("AH2");
-  //const numColumns = shitCoinsValues.length;
-  //const convertedValues = shitCoinsValues.map(value => parseFloat(value.replace('$', '')));
-  //range.offset(0, 0, 1, numColumns).setValues([convertedValues.slice(0, numColumns)]);
-
-  //add today
-  //tab.getRange("A2").setValue(formatDate(new Date()));
 }
 
 function updateUSDHISTORYtab(tab) {
@@ -207,7 +170,7 @@ function updateUSDHISTORYtab(tab) {
       "=HISTORY!X2*CURRENCY!X2", //FISH $ value
       "=HISTORY!Y2*CURRENCY!Y2", 
       "=HISTORY!Z2*CURRENCY!Z2", 
-      "=HISTORY!AA2*CURRENCY!AA2", //[SPENT USD]
+      "=0", //[SPENT USD]
       "=HISTORY!AB2*CURRENCY!AB2", 
       "=HISTORY!AC2*CURRENCY!AC2", 
       "=HISTORY!AD2*CURRENCY!AD2", 
