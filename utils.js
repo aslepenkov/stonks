a1ea1e3d5f3626971faffe76adc33a1b5d045150
Alex <alexmichel249@gmail.com>
2024-05-02 12:50:56 +0500

    add isDate1GreaterThanDate2

--- utils.js
+++ utils.js
@@ -14,6 +14,11 @@ var newColumnsIndexes = (newLst, old) =>
     .filter(i => i !== undefined)
     .map(i => startIndex + i);
 
+var isDate1GreaterThanDate2 = (date1, date2) => {
+  const d1 = new Date(date1.getFullYear(), date1.getMonth(), date1.getDate());
+  const d2 = new Date(date2.getFullYear(), date2.getMonth(), date2.getDate());
+  return d1 > d2;
+};
 
 var dailyAutofill = () => autofill(true);
 var hourlyAutofill = () => autofill(false);
